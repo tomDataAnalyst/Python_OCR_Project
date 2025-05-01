53731a583a03410f6e393eb625d141ab528e9050
Thomas Ebens <data.analyst.tom@gmail.com>
2025-05-01 16:43:58 -0400

    Initial Commit to project

---
+++ main.py
@@ -0,0 +1,4 @@
+# This is my Python OCR project to convert my D&D books and magazines from images to text, so I can both save space
+# and parse the data within more easily in preparation for database and ML.
+
+
