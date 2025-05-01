7d3aa0e79d55b8bc14184ee1d015f1532f8bed3e
Thomas Ebens <data.analyst.tom@gmail.com>
2025-05-01 17:41:26 -0400

    2nd Commit, basic OCR setup and insuring imports are working.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # This is my Python OCR project to convert my D&D books and magazines from images to text, so I can both save space
 # and parse the data within more easily in preparation for database and ML.
 
+# import cv2
+from PIL import Image
+# import pytesseract
+
+im_file = "venv/data/PHB_Test_Pg4.jpg"
+
+im = Image.open(im_file)
+# im.show()  to show in Windows Viewer
+
+
+
+
 
